b84b75263f3da53b4a4dfafbeb2ffd3e6cd173de
Soumya65567 <47179575+Soumya65567@users.noreply.github.com>
2020-07-04 10:30:13 +0530

    Add files via upload

---
+++ Array4.java
@@ -0,0 +1,17 @@
+package com.wipro.arrays;
+
+public class Array4 {
+        public static void main(String args[]){
+        	int i;
+        	char c;
+            int num[] = {65, 120, 98, 75, 115};
+            for(i=0;i<num.length;i++)
+            {
+            	c=(char)num[i];
+            	System.out.println(c);
+            }
+            
+            }
+    }
+
+
