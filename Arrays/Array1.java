6cc1a10f10356fc71fa930c7b931928b2d9a7dbc
Soumya65567 <47179575+Soumya65567@users.noreply.github.com>
2020-07-05 14:45:40 +0530

    Create Array1.java

---
+++ Arrays/Array1.java
@@ -0,0 +1,16 @@
+package com.wipro.arrays;
+
+public class Array1 {
+	public static void main(String[] args) {
+		int i,sum=0;
+		int l=args.length;
+		float avg;
+		for(i=0;i<l;i++)
+		{
+			sum=sum+Integer.parseInt(args[i]);
+		}
+		avg=sum/l;
+		System.out.println("sum is:" +sum);
+		System.out.println("avg is:" +avg);
+}
+}
