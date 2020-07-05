3ea4ddd63047122cbcb4899519fbde8d54efd005
Soumya65567 <47179575+Soumya65567@users.noreply.github.com>
2020-07-05 15:09:17 +0530

    Add files via upload

---
+++ interfaces/Veena.java
@@ -0,0 +1,11 @@
+package com.wipro.interfaces.music.String;
+import com.wipro.interfaces.music.Playable;
+public class Veena implements Playable {
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		System.out.println("Veena is being played");
+	}
+
+}
